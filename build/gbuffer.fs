de813ef56e3ba80586cd4d2c893fdb9c8d7f271a
twevs <77587819+twevs@users.noreply.github.com>
2022-12-31 17:01:07 +0000

    WIP on implementing deferred rendering.

---
+++ build/gbuffer.fs
@@ -0,0 +1,77 @@
+#version 450 core
+    
+struct Material
+{
+    sampler2D diffuse;
+    sampler2D specular;
+    sampler2D normals;
+    sampler2D displacement;
+    float shininess;
+};
+
+layout (location = 0) out vec4 positionBuffer; // Alpha = specular.
+layout (location = 1) out vec4 normalBuffer; // Alpha reserved for handedness.
+layout (location = 2) out vec4 albedoBuffer; // Alpha = shininess.
+
+#define NUM_POINTLIGHTS 4
+
+in vec3 fragPosWS;
+in vec2 texCoords;
+in mat3 tbn;
+in vec3 cameraPosTS;
+in vec3 fragPosTS;
+
+uniform Material material;
+
+// Displacement mapping.
+uniform bool displace;
+uniform float heightScale;
+
+vec2 GetDisplacedTexCoords(vec3 viewDir)
+{
+    float minLayers = 8.f;
+    float maxLayers = 32.f;
+    float numLayers = mix(maxLayers, minLayers, max(dot(vec3(0.f, 0.f, 1.f), viewDir), 0.f));
+    float layerDepth = 1.f / numLayers;
+    float curLayerDepth = 0.f;
+    vec2 p = viewDir.xy * heightScale;
+    vec2 deltaTexCoords = p / numLayers;
+    
+    vec2 result = texCoords;
+    float curMapDepth = texture(material.displacement, result).r;
+    while (curLayerDepth < curMapDepth)
+    {
+        result -= deltaTexCoords;
+        curLayerDepth += layerDepth;
+        curMapDepth = texture(material.displacement, result).r;
+    }
+    
+    vec2 prevTexCoords = result + deltaTexCoords;
+    
+    float prevDist = texture(material.displacement, prevTexCoords).r - (curLayerDepth - layerDepth);
+    float curDist = curLayerDepth - curMapDepth;
+    
+    float weight = prevDist / (curDist + prevDist);
+    result = mix(prevTexCoords, result, weight);
+    
+    return result;
+}
+
+void main()
+{    
+    vec3 cameraDir = normalize(cameraPosTS - fragPosTS);
+    vec2 displacedTexCoords = displace ? GetDisplacedTexCoords(cameraDir) : texCoords;
+    if (displacedTexCoords.x > 1.f || displacedTexCoords.y > 1.f ||
+        displacedTexCoords.x < 0.f || displacedTexCoords.y < 0.f)
+    {
+        discard;
+    }
+    vec3 norm = texture(material.normals, displacedTexCoords).rgb;
+    norm = normalize(norm * 2.f - 1.f);
+    
+    positionBuffer.rgb = fragPosWS;
+    positionBuffer.a = texture(material.specular, displacedTexCoords).r;
+    normalBuffer.rgb = tbn * norm;
+    albedoBuffer.rgb = texture(material.diffuse, displacedTexCoords).rgb;
+    albedoBuffer.a = material.shininess;
+}
---
+++ build/gbuffer.vs
@@ -0,0 +1,49 @@
+#version 450 core
+	
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
+layout (location = 3) in vec3 aTangent;
+layout (location = 4) in vec3 aBitangent;
+
+#define NUM_POINTLIGHTS 4
+
+out vec3 fragPosWS;
+out vec2 texCoords;
+out mat3 tbn;
+out vec3 cameraPosTS;
+out vec3 fragPosTS;
+
+layout (std140, binding = 0) uniform Matrices
+{
+	mat4 viewMatrix;
+	mat4 projectionMatrix;
+	mat4 dirLightSpaceMatrix;
+	mat4 spotLightSpaceMatrix;
+    mat4 pointShadowMatrices[6];
+};
+uniform mat4 modelMatrix;
+uniform mat3 normalMatrix;
+uniform vec3 cameraPos;
+uniform DirLight dirLight;
+uniform PointLight pointLights[NUM_POINTLIGHTS];
+uniform SpotLight spotLight;
+
+void main()
+{
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(aPos, 1.f);
+    fragPosWS = vec3(modelMatrix * vec4(aPos, 1.f));
+    texCoords = aTexCoords;
+	
+	// NOTE: Assimp's aiProcess_CalcTangentSpace produces unit vectors.
+	vec3 tangent = normalize(vec3(modelMatrix * vec4(aTangent, 0.f)));
+	vec3 norm = normalize(vec3(modelMatrix * vec4(aNormal, 0.f)));
+	tangent = normalize(tangent - dot(tangent, norm) * norm);
+	// vec3 bitangent = normalize(vec3(modelMatrix * vec4(aBitangent, 0.f)));
+	vec3 bitangent = cross(norm, tangent);
+	tbn = mat3(tangent, bitangent, norm);
+	
+	mat3 invTbn = transpose(tbn);
+	cameraPosTS = invTbn * cameraPos;
+	fragPosTS = invTbn * fragPosWS;
+}
